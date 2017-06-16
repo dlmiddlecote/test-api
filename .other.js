41bbe48e075a0b2d229d25477cb29750dad3f5b7
Daniel Middlecote <daniel.middlecote@playtech.com>
2017-06-16 16:00:21 +0100

    Fix tests

--- .other.js
+++ .other.js
@@ -38,3 +38,36 @@ describe("resources", function() {
       });
   });
 });
+
+describe('creating resources', function() {
+  it('should create and fetch a new resource', function(done) {
+    // send a POST request to create a new resource
+	// send a GET request for the new id, and check that the resource saved
+	// is the one we get back
+    api.post('/resources')
+      .send({
+        forename: "harry",
+        surname: "potter"
+      })
+      .set('Accept', 'application/json')
+      .expect(201)
+      .end(function(err, res) {
+         if (err) return done(err);
+
+		 var id = res.body.id;
+		 api.get('/resources/' + id)
+		  .set('Accept', 'application/json')
+		  .expect(200)
+		  .end(function(err, res) {
+			if (err) return done(err);
+
+			expect(res.body.forename).to.be.equal('harry');
+			expect(res.body.surname).to.be.equal('potter');
+
+			done();
+		  });
+	  });
+  });
+});
+
+
